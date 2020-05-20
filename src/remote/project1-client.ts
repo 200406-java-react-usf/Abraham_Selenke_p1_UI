e34d001ae73b3120aff886bbf73b15b8fa496725
Abraham Selenke <slinky@Abraham.local>
2020-05-19 18:13:34 -0600

    Tetsing AWS

--- src/remote/project1-client.ts
+++ src/remote/project1-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const project1Client = axios.create({
-    //baseURL: 'http://aselenkeproject1api-env.eba-cdvh8tyx.us-east-2.elasticbeanstalk.com/',
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://aselenkeproject1api-env.eba-cdvh8tyx.us-east-2.elasticbeanstalk.com/',
+    //baseURL: 'http://localhost:8080',
     headers: {
         'Content-Type': 'application/json'
     }, 
