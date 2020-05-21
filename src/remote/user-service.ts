160ca70069a3d9d9bd2d2d19b691f03fcb535dfe
Abraham Selenke <slinky@Abraham.local>
2020-05-21 15:59:28 -0600

    Having issues with connection, should all be fixed

--- src/remote/user-service.ts
+++ src/remote/user-service.ts
@@ -1,7 +1,7 @@
 import { project1Client } from "./project1-client";
 
 export async function newUser(username: string, password: string, firstName: string, lastName: string, email: string) {
-    let response = await project1Client.post('/register', {username, password, firstName, lastName, email});
+    let response = await project1Client.post('/users', {username, password, firstName, lastName, email});
     return await response.data;
 }
 
